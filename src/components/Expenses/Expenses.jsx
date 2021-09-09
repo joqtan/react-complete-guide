4aa27f3ae0570034f2851d78ef6eea4d60e82e03
Joqtan Marmol <joqtanmarmol@gmail.com>
2021-09-09 14:41:26 -0600

    until class 44

---
+++ src/components/Expenses/Expenses.jsx
@@ -0,0 +1,19 @@
+import Card from '../UI/Card'
+import ExpenseItem from './ExpenseItem'
+import './Expenses.css'
+
+const Expenses = (props) => {
+  const expenses = props.expenses
+  return (
+    <Card className="expenses">
+      {expenses.map((expense) => (
+        <ExpenseItem
+          date={expense.date}
+          title={expense.title}
+          amount={expense.amount}
+        />
+      ))}
+    </Card>
+  )
+}
+export default Expenses
